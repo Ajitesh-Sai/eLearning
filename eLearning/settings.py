29ed559c0a9fa68aa9cdda94b4e0f5e73023f2b1
Sathyasai Ajitesh <ajiteshsai@gmail.com>
2020-07-31 21:53:11 +0530

    First Commit

--- eLearning/settings.py
+++ eLearning/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '848o#$#_obr+1^@ky3&61_3n9lwjz3(k8-e%n(lg-)xjfwqfw&'
+#SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
